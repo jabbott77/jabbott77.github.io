cfc347eb3297a1c32a681f389b3d2587a05b5a5b
Use your GitHub username <Enter your email address>
2020-11-18 12:53:23 -0700

    storm

---
+++ Lesson 8/js/storm.js
@@ -0,0 +1,32 @@
+window.addEventListener('load',(event)=>{
+    // add code here to run when page loads
+    const menubutton = document.querySelector('.menu');
+    const mainnav = document.querySelector('.navigation')   
+    
+    menubutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+    
+    // To solve the mid resizing issue with responsive class on
+    window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+    
+    /*** Programming Notes **************************************
+      Arrow Functions - es6 syntactically compact alternative to a regular function expression
+      see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+      or https://www.w3schools.com/js/js_arrow_function.asp
+    
+      classList property - https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
+      */
+})
+
+window.addEventListener('load', (event)=>{
+    const lu = document.querySelector('#lastupdated');
+    lu.textContent = document.lastModified;
+
+    const cry = document.querySelector("#copyrightyear");
+    cry.textContent = new Date().getFullYear();
+
+    const b =document.querySelector("#banner");
+    today = new Date().getDay();
+    if (today==5){
+        b.style.display = "block";
+    }
+});
